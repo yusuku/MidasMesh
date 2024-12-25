959a62988b733ee9a6300922bd6fd74451d7b208
yusuku <121999446+yusuku@users.noreply.github.com>
2024-12-25 21:15:16 +0900

    Add files via upload

---
+++ DepthRefine.cs
@@ -0,0 +1,29 @@
+using Unity.Sentis;
+using UnityEngine;
+
+public class DepthRefine : MonoBehaviour
+{
+    public Texture2D inputTexture;
+    //Midas
+    public ModelAsset modelAsset;
+    public Material Debugmat;
+    MidasEstimation midas;
+    RenderTexture outputTexture;
+
+    public Material TrDebugmat;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+
+        midas=new MidasEstimation(modelAsset,Debugmat);
+        midas.inference(inputTexture);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
